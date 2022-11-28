845f80ed4cc13dc794b3b9332a6ddf6e84e58abd
Bruce Scharlau <b.scharlau@abdn.ac.uk>
2022-11-28 09:41:18 +0000

    cleaned up exercise

--- cities/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.city_list, name='city_list'),
-    path('cities/<str:city>', views.city__by_name, name='cityname'),
-    ]
